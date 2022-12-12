ade5881227e9b0208f53d982da81547aaf9f0434
tomo(mac) <tomomo50@gmail.com>
2022-12-12 21:42:17 +0900

    商品関連

---
+++ resources/js/Components/FlashMessage.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { computed } from 'vue';
+
+</script>
+
+<template>
+    <div v-if="$page.props.flash.status === 'success'" class="bg-blue-300">
+        {{ $page.props.flash.message }}
+    </div>
+    <div v-if="$page.props.flash.status === 'danger'" class="bg-red-300">
+        {{ $page.props.flash.message }}
+    </div>
+</template>
